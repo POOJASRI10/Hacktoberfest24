72dee339c51d725471474bb36e66c523212dedc2
Pooja <youremail@example.com>
2024-10-18 19:48:35 +0530

    matrix multiplication

--- Matrix multiplication.cpp
+++ Matrix multiplication.cpp
@@ -1,52 +1,73 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-// mat1[R1][C1] and mat2[R2][C2]
-#define R1 2 // number of rows in Matrix-1
-#define C1 2 // number of columns in Matrix-1
-#define R2 2 // number of rows in Matrix-2
-#define C2 2 // number of columns in Matrix-2
+void multiplyMatrices(int A[][10], int B[][10], int result[][10], int r1, int c1, int r2, int c2) {
+    // Initialize the result matrix to 0
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            result[i][j] = 0;
+        }
+    }
 
-void mulMat(int mat1[][C1], int mat2[][C2])
-{
-    int rslt[R1][C2];
-
-    cout << "Multiplication of given two matrices is:\n";
-
-    for (int i = 0; i < R1; i++) {
-        for (int j = 0; j < C2; j++) {
-            rslt[i][j] = 0;
-
-            for (int k = 0; k < R2; k++) {
-                rslt[i][j] += mat1[i][k] * mat2[k][j];
+    // Perform matrix multiplication
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            for (int k = 0; k < c1; k++) {
+                result[i][j] += A[i][k] * B[k][j];
             }
-            cout << rslt[i][j] << "\t";
+        }
+    }
+}
+
+void displayMatrix(int matrix[][10], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << " ";
         }
         cout << endl;
     }
 }
 
-// Driver code
-int main()
-{
-    int mat1[R1][C1] = { { 1, 3 },
-                         { 2, 4 } };
+int main() {
+    int r1, c1, r2, c2;
 
-    int mat2[R2][C2] = { { 1, 2 },
-                         { 3, 4 } };
+    // Input dimensions of the matrices
+    cout << "Enter rows and columns for the first matrix: ";
+    cin >> r1 >> c1;
+    cout << "Enter rows and columns for the second matrix: ";
+    cin >> r2 >> c2;
 
-    if (C1 != R2) {
-        cout << "The number of columns in Matrix-1  must "
-                "be equal to the number of rows in "
-                "Matrix-2"
-             << endl;
-        cout << "Please update MACROs according to your "
-                "array dimension in #define section"
-             << endl;
-
-        exit(EXIT_FAILURE);
+    // Check if matrix multiplication is possible
+    if (c1 != r2) {
+        cout << "Matrix multiplication not possible. Number of columns in the first matrix must match the number of rows in the second matrix." << endl;
+        return 0;
     }
-    mulMat(mat1, mat2);
+
+    // Declare matrices
+    int A[10][10], B[10][10], result[10][10];
+
+    // Input first matrix
+    cout << "Enter elements of the first matrix:" << endl;
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            cin >> A[i][j];
+        }
+    }
+
+    // Input second matrix
+    cout << "Enter elements of the second matrix:" << endl;
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
+            cin >> B[i][j];
+        }
+    }
+
+    // Multiply matrices
+    multiplyMatrices(A, B, result, r1, c1, r2, c2);
+
+    // Display the result
+    cout << "Resultant matrix after multiplication:" << endl;
+    displayMatrix(result, r1, c2);
 
     return 0;
 }
